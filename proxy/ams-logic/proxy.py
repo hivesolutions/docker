288965ab16ea966519de5d46ca4e3725c503792e
joamag <joamag@gmail.com>
2015-12-09 09:27:01 +0000

    new proxy configuration

--- proxy/ams-logic/proxy.py
+++ proxy/ams-logic/proxy.py
@@ -2,7 +2,8 @@ import logging
 import netius.extra
 
 hosts = {
-    "cameria.bemisc.com" : "http://hive-neo-9c45d442.hivesolutions.svc.tutum.io:8001"
+    "hive.pt" : "http://hive-neo-9c45d442.hivesolutions.svc.tutum.io:8001/",
+    "cameria.bemisc.com" : "http://cameria-adc13c3f.hivesolutions.svc.tutum.io:8002/"
 }
 server = netius.extra.ReverseProxyServer(
     hosts = hosts

8fcd6585975c7616014befb4c56eebca34ae9672
joamag <joamag@hive.pt>
2015-12-11 17:04:42 +0000

    new structure

--- proxy/ams-logic/proxy.py
+++ proxy/ams-logic/proxy.py
@@ -3,6 +3,7 @@ import netius.extra
 
 hosts = {
     "hive.pt" : "http://hive-neo-9c45d442-1.hivesolutions.cont.tutum.io:8001",
+    "www.hive.pt" : "http://hive-neo-9c45d442-1.hivesolutions.cont.tutum.io:8001",
     "cameria.bemisc.com" : "http://cameria-adc13c3f-1.hivesolutions.cont.tutum.io:8002",
     "crossline.bemisc.com" : "http://crossline-d3f0b911-1.hivesolutions.cont.tutum.io:8003",
     "omnix.bemisc.com" : "http://omnix-0743d1f2-1.hivesolutions.cont.tutum.io:8004",
@@ -11,7 +12,14 @@ hosts = {
     "campaigner.bemisc.com" : "http://campaigner-4a9f12ed-1.hivesolutions.cont.tutum.io:8007",
     "instashow.bemisc.com" : "http://instashow-4a9f12ed-1.hivesolutions.cont.tutum.io:8008"
 }
+regex = (
+    (re.compile("https?://hive\.pt"), "http://hive-neo-9c45d442-1.hivesolutions.cont.tutum.io:8001"),
+    (re.compile("https?://www.hive\.pt"), "http://hive-neo-9c45d442-1.hivesolutions.cont.tutum.io:8001"),
+    (re.compile("https?://([a-zA-Z_]+)\.hive\.pt"), "http://127.0.0.1:8181"),
+    (re.compile(".*"), "http://127.0.0.1:8181")
+)
 server = netius.extra.ReverseProxyServer(
-    hosts = hosts
+    hosts = hosts,
+    regex = regex
 )
 server.serve(env = True)

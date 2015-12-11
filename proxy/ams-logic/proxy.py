ef8438b15ed44e297fc3dde04cdf5123617e6bd1
joamag <joamag@hive.pt>
2015-12-11 14:03:40 +0000

    new campaigner support

--- proxy/ams-logic/proxy.py
+++ proxy/ams-logic/proxy.py
@@ -7,7 +7,8 @@ hosts = {
     "crossline.bemisc.com" : "http://crossline-d3f0b911.hivesolutions.svc.tutum.io:8003/",
     "omnix.bemisc.com" : "http://omnix-0743d1f2.hivesolutions.svc.tutum.io:8004/",
     "lugardajoia.com" : "http://websites-97afa40c.hivesolutions.svc.tutum.io:8005/",
-    "passatempo.oibiquini.com" : "http://websites-353a812f.hivesolutions.svc.tutum.io:8006/"
+    "passatempo.oibiquini.com" : "http://websites-353a812f.hivesolutions.svc.tutum.io:8006/",
+    "campaigner.bemisc.com" : "http://campaigner-4a9f12ed.hivesolutions.svc.tutum.io:8007/"
 }
 server = netius.extra.ReverseProxyServer(
     hosts = hosts

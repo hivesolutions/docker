9ef12ebf12745e9808e5e863c78a01ccb146c8e9
joamag <joamag@hive.pt>
2015-12-11 16:11:05 +0000

    new proxy value

--- proxy/ams-logic/proxy.py
+++ proxy/ams-logic/proxy.py
@@ -2,14 +2,14 @@ import logging
 import netius.extra
 
 hosts = {
-    "hive.pt" : "http://hive-neo-9c45d442-1.hivesolutions.cont.tutum.io:8001/",
-    "cameria.bemisc.com" : "http://cameria-adc13c3f-1.hivesolutions.cont.tutum.io:8002/",
-    "crossline.bemisc.com" : "http://crossline-d3f0b911-1.hivesolutions.cont.tutum.io:8003/",
-    "omnix.bemisc.com" : "http://omnix-0743d1f2-1.hivesolutions.cont.tutum.io:8004/",
-    "lugardajoia.com" : "http://websites-97afa40c-1.hivesolutions.cont.tutum.io:8005/",
-    "passatempo.oibiquini.com" : "http://websites-353a812f-1.hivesolutions.cont.tutum.io:8006/",
-    "campaigner.bemisc.com" : "http://campaigner-4a9f12ed-1.hivesolutions.cont.tutum.io:8007/",
-    "instashow.bemisc.com" : "http://instashow-4a9f12ed-1.hivesolutions.cont.tutum.io:8008/"
+    "hive.pt" : "http://hive-neo-9c45d442-1.hivesolutions.cont.tutum.io:8001",
+    "cameria.bemisc.com" : "http://cameria-adc13c3f-1.hivesolutions.cont.tutum.io:8002",
+    "crossline.bemisc.com" : "http://crossline-d3f0b911-1.hivesolutions.cont.tutum.io:8003",
+    "omnix.bemisc.com" : "http://omnix-0743d1f2-1.hivesolutions.cont.tutum.io:8004",
+    "lugardajoia.com" : "http://websites-97afa40c-1.hivesolutions.cont.tutum.io:8005",
+    "passatempo.oibiquini.com" : "http://websites-353a812f-1.hivesolutions.cont.tutum.io:8006",
+    "campaigner.bemisc.com" : "http://campaigner-4a9f12ed-1.hivesolutions.cont.tutum.io:8007",
+    "instashow.bemisc.com" : "http://instashow-4a9f12ed-1.hivesolutions.cont.tutum.io:8008"
 }
 server = netius.extra.ReverseProxyServer(
     hosts = hosts

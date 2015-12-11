c3a93313e1078ac038bcb55b3acef4d8e1440b1c
joamag <joamag@hive.pt>
2015-12-11 15:45:46 +0000

    small code fix

--- proxy/ams-logic/proxy.py
+++ proxy/ams-logic/proxy.py
@@ -7,8 +7,9 @@ hosts = {
     "crossline.bemisc.com" : "http://crossline-d3f0b911-1.hivesolutions.cont.tutum.io:8003/",
     "omnix.bemisc.com" : "http://omnix-0743d1f2-1.hivesolutions.cont.tutum.io:8004/",
     "lugardajoia.com" : "http://websites-97afa40c-1.hivesolutions.cont.tutum.io:8005/",
-    "passatempo.oibiquini.com" : "http://websites-353a812f-1.hivesolutions.cont.tutum.io:8006/"
-    "campaigner.bemisc.com" : "http://campaigner-4a9f12ed-1.hivesolutions.cont.tutum.io:8007/"
+    "passatempo.oibiquini.com" : "http://websites-353a812f-1.hivesolutions.cont.tutum.io:8006/",
+    "campaigner.bemisc.com" : "http://campaigner-4a9f12ed-1.hivesolutions.cont.tutum.io:8007/",
+    "instashow.bemisc.com" : "http://instashow-4a9f12ed-1.hivesolutions.cont.tutum.io:8008/"
 }
 server = netius.extra.ReverseProxyServer(
     hosts = hosts

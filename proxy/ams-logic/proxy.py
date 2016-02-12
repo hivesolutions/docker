9c7d2b997c7aeb8d1c7d660e3fc7f4c249420182
joamag <joamag@hive.pt>
2016-02-12 16:40:39 +0000

    new oibiquini extras

--- proxy/ams-logic/proxy.py
+++ proxy/ams-logic/proxy.py
@@ -17,7 +17,7 @@ hosts = {
     "campaigner.bemisc.com" : "http://campaigner-4a9f12ed-1.hivesolutions.cont.tutum.io:8007",
     "instashow.bemisc.com" : "http://instashow-17632612-1.hivesolutions.cont.tutum.io:8008",
     "libs.bemisc.com" : "http://libs-2ca151e2-1.hivesolutions.cont.tutum.io:8009",
-    "extras.oibiquini.com" : "http://oibiquini-extras-a9d0f203-1.hivesolutions.cont.tutum.io:8010",
+    "extras.oibiquini.com" : "http://oibiquini-extras-24badc01-1.hivesolutions.cont.tutum.io:8010",
     "repos.bemisc.com" : "http://repos-a2dd9087-1.hivesolutions.cont.tutum.io:8011",
     "colony.bemisc.com" : "http://repos-a2dd9087-1.hivesolutions.cont.tutum.io:8011",
     "proyectos.bemisc.com" : "http://proyectos-e2b381d1-1.hivesolutions.cont.tutum.io:8012",

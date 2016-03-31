2ce5986d3b1fe974e31a6241eb376544a8c7f687
joamag <joamag@hive.pt>
2016-03-31 12:43:32 +0100

    new values

--- builder/run.sh
+++ builder/run.sh
@@ -10,7 +10,7 @@ FULL_NAME=${FULL_NAME-$REPO/$NAME}
 echo "Building '$NAME' using '$BUILDER' and '$EXEC'"
 
 if [ "$BUILDER" == "Dockerfile" ]; then
-    docker build --no-cache -t $FULL_NAME $EXEC
+    docker build --no-cache -t $FULL_NAME ./$EXEC
 else
     NAME=$NAME FULL_NAME=$FULL_NAME BUILDER=$BUILDER /repo/$EXEC
 fi

1d60e6fd95ef9ee42520abe4fc70441567bada0a
joamag <joamag@hive.pt>
2016-03-31 12:29:56 +0100

    new runner

--- docker_build/run.sh
+++ docker_build/run.sh
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 
 NAME=${NAME-$(date +%s)}
+FULL_NAME=${FULL_NAME-$REPO/$NAME}
 
 echo "Building '$NAME' using '$BUILDER' and '$EXEC'"
 
 if [ "$BUILDER" == "Dockerfile" ]; then
-    docker build $EXEC
+    docker build $EXEC --tag=$FULL_NAME
 else
-    NAME=$NAME BUILDER=$BUILDER /repo/$EXEC
+    NAME=$NAME FULL_NAME=$FULL_NAME BUILDER=$BUILDER /repo/$EXEC
 fi
+
+docker push $FULL_NAME

a30881b4efa3ae22f76f10a11a5cfa4d27d5e5fa
joamag <joamag@hive.pt>
2016-03-31 13:04:46 +0100

    small code change

--- builder/run.sh
+++ builder/run.sh
@@ -9,6 +9,7 @@ FULL_NAME=${FULL_NAME-$REPO/$NAME}
 
 echo "Building '$FULL_NAME' using '$BUILDER' and '$EXEC'"
 
+mkdir -p /lib/modules
 cgroupfs.mount && docker daemon -s overlay &
 
 if [ "$BUILDER" == "Dockerfile" ]; then

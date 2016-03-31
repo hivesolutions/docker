cebf21a67925256ac77d4a0133689f23ee1e0de2
joamag <joamag@hive.pt>
2016-03-31 13:42:39 +0100

    new mounting

--- builder/run.sh
+++ builder/run.sh
@@ -10,6 +10,7 @@ FULL_NAME=${FULL_NAME-$REPO/$NAME}
 echo "Building '$FULL_NAME' using '$BUILDER' and '$EXEC'"
 
 mkdir -p /lib/modules
+mount -t cgroup devices 1 /sys/fs/cgroup
 docker daemon -s overlay &
 
 if [ "$BUILDER" == "Dockerfile" ]; then

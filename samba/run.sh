0bdcab34e9992562307e0f7568cf0a9940db87f5
joamag <joamag@gmail.com>
2017-03-28 18:33:37 +0100

    new code

--- samba/run.sh
+++ samba/run.sh
@@ -6,7 +6,12 @@ set -e +h
 sed -ie "s/{ENCRYPTION}/$ENCRYPTION/g" /etc/samba/smb.conf
 sed -ie "s/{SIGNING}/$SIGNING/g" /etc/samba/smb.conf
 
-adduser -D samba
+getent passwd samba > /dev/null 2&>1
+
+if [ $? -eq 0 ]; then
+else
+    adduser -D samba
+fi
 
 (echo $PASSWORD && echo $PASSWORD) | smbpasswd -a samba
 

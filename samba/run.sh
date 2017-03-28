e8b9900ff5e66c3fbd5849c0b047033a1e3c21d1
joamag <joamag@gmail.com>
2017-03-28 18:47:19 +0100

    new run script

--- samba/run.sh
+++ samba/run.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
-set -e +h
-
 sed -ie "s/{ENCRYPTION}/$ENCRYPTION/g" /etc/samba/smb.conf
 sed -ie "s/{SIGNING}/$SIGNING/g" /etc/samba/smb.conf
 
 getent passwd samba > /dev/null 2&>1
-
 if [ ! $? -eq 0 ]; then
     adduser -D samba
 fi

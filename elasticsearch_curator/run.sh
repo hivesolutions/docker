398c392f6288ff133e842cd8c3792ad0d6551fcf
joamag <joamag@gmail.com>
2018-11-05 21:13:43 +0000

    new curator host value

--- elasticsearch_curator/run.sh
+++ elasticsearch_curator/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
-curator_cli delete_indices --filter_list "{\"filtertype\":\"age\",\"source\":\"name\",\"timestring\":\"%Y.%m.%d\",\"unit\":\"$CURATOR_UNIT\",\"unit_count\":$CURATOR_COUNT}"
+curator_cli delete_indices --host "$CURATOR_HOST" --filter_list "{\"filtertype\":\"age\",\"source\":\"name\",\"timestring\":\"%Y.%m.%d\",\"unit\":\"$CURATOR_UNIT\",\"unit_count\":$CURATOR_COUNT}"

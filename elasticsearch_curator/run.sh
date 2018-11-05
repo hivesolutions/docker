6d595a41cec4a314ad99cb5a31f6b5e50c7f4985
joamag <joamag@gmail.com>
2018-11-05 22:11:42 +0000

    new direction value

--- elasticsearch_curator/run.sh
+++ elasticsearch_curator/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
-curator_cli --host "$CURATOR_HOST" delete_indices --filter_list "{\"filtertype\":\"age\",\"source\":\"name\",\"timestring\":\"%Y.%m.%d\",\"unit\":\"$CURATOR_UNIT\",\"unit_count\":$CURATOR_COUNT}"
+curator_cli --host "$CURATOR_HOST" delete_indices --filter_list "{\"filtertype\":\"age\",\"source\":\"name\",\"timestring\":\"%Y.%m.%d\",\"unit\":\"$CURATOR_UNIT\",\"unit_count\":$CURATOR_COUNT,\"direction\":\"older\"}"

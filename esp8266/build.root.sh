82e3bd19d30ffca6d217a9df758b92bb86a0fb14
joamag <joamag@hive.pt>
2019-06-04 12:04:19 +0100

    New curl added

--- esp8266/build.root.sh
+++ esp8266/build.root.sh
@@ -8,6 +8,6 @@ echo "Installing esp-open-sdk, Espressif ESP-IDF, and micropython dependencies..
 echo "Europe/London" > /etc/timezone
 
 apt-get update
-apt-get install -y -q build-essential git make unrar-free unzip\
+apt-get install -y -q build-essential git make unrar-free unzip curl\
     autoconf automake libtool libtool-bin gcc g++ gperf flex bison texinfo gawk ncurses-dev libexpat-dev\
     python python3 sed libreadline-dev libffi-dev pkg-config help2man python-dev python-serial wget sudo

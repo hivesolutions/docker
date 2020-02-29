7ddcbe5b8e1fcef154c6cc6b1d66ab594ae6e3e5
João Magalhães <joamag@gmail.com>
2020-02-29 17:32:13 +0000

    New micropython version based

--- esp8266/build.sh
+++ esp8266/build.sh
@@ -3,6 +3,7 @@
 
 export XTENSA_FILE="xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz"
 export IDF_VERSION="v3.3.1"
+export MICROPYTHON_VERSION="v1.12"
 
 cd ~
 
@@ -24,8 +25,8 @@ echo "PATH=$(pwd)/esp-open-sdk/xtensa-lx106-elf/bin:\$PATH" >> ~/.profile
 source ~/.profile
 
 git clone -b $IDF_VERSION --recursive https://github.com/espressif/esp-idf.git
+git clone -b $MICROPYTHON_VERSION --recursive https://github.com/micropython/micropython.git
 
-git clone --recursive https://github.com/micropython/micropython.git
 pushd micropython
 make -C mpy-cross
 popd

13319891cd933c876f736b240cc921913ff5f273
João Magalhães <joamag@gmail.com>
2020-09-28 20:39:35 +0100

    Small fix

--- esp8266/build.sh
+++ esp8266/build.sh
@@ -18,7 +18,7 @@ rm $XTENSA_FILE
 echo "Installing ESP Open SDK..."
 
 git clone --recursive https://github.com/pfalcon/esp-open-sdk.git
-pushd crosstool-NG|
+pushd crosstool-NG
 git checkout xtensa-1.22.x
 popd
 pushd esp-open-sdk

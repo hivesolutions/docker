486e34830e1e24d41e5ebe1042d8cb7d447fe6ac
João Magalhães <joamag@gmail.com>
2022-12-28 17:05:18 +0000

    feat: major simplification of ESP Open SDK installation Using pre-built binaries for the installation

--- esp8266/build.root.sh
+++ esp8266/build.root.sh
@@ -6,16 +6,9 @@ set -e +h
 export TZ=Europe/London
 export DEBIAN_FRONTEND=noninteractive
 
-echo "Installing esp-open-sdk, Espressif ESP-IDF, and micropython dependencies..."
+echo "Installing esp-open-sdk, Espressif ESP-IDF, and MicroPython dependencies..."
 echo "Europe/London" > /etc/timezone
 
 apt-get update
-apt-get install -y -q build-essential git make unrar-free unzip curl\
-    autoconf automake libtool libtool-bin gcc g++ gperf flex bison texinfo gawk ncurses-dev libexpat-dev\
-    python2 python3 sed libreadline-dev libffi-dev pkg-config help2man python2-dev python3-dev python3-serial wget sudo
-
-ln -s /usr/bin/python2 /usr/bin/python
-
-wget "https://bootstrap.pypa.io/pip/2.7/get-pip.py"
-python get-pip.py
-pip install serial
+apt-get install -y -q git wget flex bison gperf python3 python3-pip\
+    python3-setuptools cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
--- esp8266/build.sh
+++ esp8266/build.sh
@@ -3,45 +3,35 @@
 
 set -e +h
 
-export XTENSA_FILE="xtensa-esp32-elf-linux64-1.22.0-97-gc752ad5-5.2.0.tar.gz"
-export XTENSA_VERSION="1.22.x"
+export ESP_OPEN_SDK_VERSION="2018-06-10"
 export IDF_VERSION="v3.3.6"
-export ESP_OPEN_SDK_VERSION="sdk2-lwip212"
-export MICROPYTHON_VERSION="v1.19.1"
+export MICROPYTHON_VERSION="v1.13"
 
 cd ~
 
-echo "Installing Espressif ESP32 toolchain..."
-
-wget https://dl.espressif.com/dl/$XTENSA_FILE
-tar -zxvf $XTENSA_FILE
-rm $XTENSA_FILE
-
 echo "Installing ESP Open SDK..."
 
-git clone -b $ESP_OPEN_SDK_VERSION --recursive https://github.com/someburner/esp-open-sdk
-pushd esp-open-sdk
-pushd crosstool-ng
-git checkout xtensa-$XTENSA_VERSION
-popd
-make STANDALONE=y
-
-# fixes invalid path of lwip_open library which is meant to be found
-# under `usr/lib` by the linker, otherwise linking issues would occur
-# when trying to build the Micropython firmware
-cp -p xtensa-lx106-elf/xtensa-lx106-elf/sysroot/lib/liblwip_open.a xtensa-lx106-elf/xtensa-lx106-elf/sysroot/usr/lib
-
-popd
+# obtains the pre-built version of the ESP Open SDK and unpacks it
+# putting it in the expected location
+wget https://github.com/jepler/esp-open-sdk/releases/download/$ESP_OPEN_SDK_VERSION/xtensa-lx106-elf-standalone.tar.gz
+tar -zxf xtensa-lx106-elf-standalone.tar.gz
+rm xtensa-lx106-elf-standalone.tar.gz
 
 # adds the binaries to the path so that they can be used directly
 # in the new bash processed from now on (xtensa-lx106 refers to
 # the ESP 8266 architecture)
-echo "PATH=$(pwd)/xtensa-esp32-elf/bin:\$PATH" >> ~/.profile
-echo "PATH=$(pwd)/esp-open-sdk/xtensa-lx106-elf/bin:\$PATH" >> ~/.profile
+echo "PATH=$(pwd)/xtensa-lx106-elf/bin:\$PATH" >> ~/.profile
 source ~/.profile
 
+# clons both the ESP IDF repository and the MicroPython one so that
+# they can be latter used for MicroPython related activities
 git clone -b $IDF_VERSION --recursive https://github.com/espressif/esp-idf.git
 git clone -b $MICROPYTHON_VERSION --recursive https://github.com/micropython/micropython.git
+
+# patches the "etshal.h" file to make it compatible with more up-to-date
+# Open SDKs 2.2+ which have different signatures for some methods
+#sed -i -e 's/void ets_delay_us(uint16_t us);/void ets_delay_us(uint32_t us);/g' micropython/ports/esp8266/etshal.h
+sed -i -e 's/void ets_delay_us(uint32_t us);/void ets_delay_us(uint16_t us);/g' micropython/ports/esp8266/etshal.h
 
 pushd micropython
 make -C mpy-cross

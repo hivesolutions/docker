5e19e322763f0f7053a8d1a1a9b9938bf7f7db6c
João Magalhães <joamag@gmail.com>
2022-12-28 17:26:48 +0000

    fix: esp8266 structure

--- esp8266/build.root.sh
+++ esp8266/build.root.sh
@@ -10,5 +10,11 @@ echo "Installing esp-open-sdk, Espressif ESP-IDF, and MicroPython dependencies..
 echo "Europe/London" > /etc/timezone
 
 apt-get update
-apt-get install -y -q git wget flex bison gperf python3 python3-pip\
+apt-get install -y -q git wget flex bison gperf python2 python3 python3-pip\
     python3-setuptools cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
+
+ln -s /usr/bin/python2 /usr/bin/python
+
+wget "https://bootstrap.pypa.io/pip/2.7/get-pip.py"
+python get-pip.py
+pip install serial
